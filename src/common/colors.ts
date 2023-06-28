850750300b91e21b230176f7db616897eb09cec3
Anderson R. Vanzo <anderson.vanzo01@gmail.com>
2023-06-28 12:16:43 -0300

    feat(common): colors configuration

---
+++ src/common/colors.ts
@@ -0,0 +1,13 @@
+export const colors = {
+  primary: '#4485FD',
+  secondary: '#00CC6A',
+
+  title: '#25282B',
+  text: '#C4C4C4',
+  button_label: '#FFFFFF',
+
+  secondary_light: '#CCF5E1',
+  title_light: '#A0A4A8',
+
+  background: '#FFFFFF',
+};
