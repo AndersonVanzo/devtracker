11fa92ccbf8e1376b1aa04c3aea009a69e792070
Anderson R. Vanzo <anderson.vanzo01@gmail.com>
2023-06-28 12:18:21 -0300

    fix(components/screencontainer): setting background color based on configuration file

--- src/components/screen-container/styles.ts
+++ src/components/screen-container/styles.ts
@@ -1,7 +1,9 @@
 import { StyleSheet } from 'react-native';
+import { colors } from '../../common/colors';
 
 export const styles = StyleSheet.create({
   container: {
+    backgroundColor: colors.background,
     flex: 1,
     padding: 24,
   },
