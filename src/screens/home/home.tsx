483a9b2227deee760d5822583ee9b6c208d3164c
Anderson R. Vanzo <anderson.vanzo01@gmail.com>
2023-06-28 13:25:04 -0300

    feat(screens): home screen

--- src/screens/home/home.tsx
+++ src/screens/home/home.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import ScreenContainer from '../../components/screen-container/screencontainer';
+import PageTitle from '../../components/page-title/pagetitle';
+import SearchBar from '../../components/search-bar/searchbar';
+import Button from '../../components/button/button';
 import { View } from 'react-native';
+import { styles } from './styles';
 
 const Home = () => {
-  return <View />;
+  return (
+    <ScreenContainer>
+      <View style={styles.content}>
+        <PageTitle title={'Find'} effectTitle={'a dev'} />
+        <SearchBar value={''} placeholder={'Search a dev'} onChangeValue={() => console.log()} />
+        <Button label={'Find'} onPress={() => console.log()} />
+      </View>
+    </ScreenContainer>
+  );
 };
 
 export default Home;
