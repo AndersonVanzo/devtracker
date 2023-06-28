7ba966ef74163c0b7f6bfa19f5856573dac941cd
Anderson R. Vanzo <anderson.vanzo01@gmail.com>
2023-06-28 14:02:41 -0300

    feat(components/button): added option to customize the background color

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -5,11 +5,12 @@ import { styles } from './styles';
 interface ButtonProps {
   label: string;
   onPress: () => void;
+  customColor?: string;
 }
 
-const Button = ({ label, onPress }: ButtonProps) => {
+const Button = ({ label, onPress, customColor }: ButtonProps) => {
   return (
-    <TouchableOpacity activeOpacity={0.75} style={styles.button} onPress={onPress}>
+    <TouchableOpacity activeOpacity={0.75} style={[styles.button, customColor ? { backgroundColor: customColor } : {}]} onPress={onPress}>
       <Text style={styles.label}>{label}</Text>
     </TouchableOpacity>
   );
