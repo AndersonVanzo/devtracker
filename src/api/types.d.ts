28c41316a3b0b0bf587f6eaf2f83a1d890af2644
Anderson R. Vanzo <anderson.vanzo01@gmail.com>
2023-06-28 15:29:43 -0300

    feat(api): basic configuration

---
+++ src/api/types.d.ts
@@ -0,0 +1,26 @@
+export {};
+
+declare global {
+  const ApiStatus = {
+    OK = 200,
+    CREATED = 201,
+    NO_CONTENT = 204,
+    BAD_REQUEST = 400,
+    UNAUTHORIZED = 401,
+    FORBIDDEN = 403,
+    NOT_FOUND = 404,
+    INTERNAL_SERVER_ERROR = 500,
+    BAD_GATEWAY = 502,
+    SERVICE_UNAVAILABLE = 503,
+    GATEWAY_TIMEOUT = 504,
+  } as const;
+
+  type ApiStatus = (typeof ApiStatus)[keyof typeof ApiStatus];
+
+  interface ApiResponse<Type> {
+    data: Type;
+    message: string;
+    status: ApiStatus;
+    success: boolean;
+  }
+}
