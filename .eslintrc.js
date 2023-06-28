9242ac1b4841311591fb9289e6622d7a6485c6a0
Anderson R. Vanzo <anderson.vanzo01@gmail.com>
2023-06-28 11:06:36 -0300

    feat: eslint and prettier configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,9 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: '@react-native-community',
+  rules: {
+    'object-curly-spacing': 0,
+    'no-control-regex': 0,
+    'react/no-unstable-nested-components': ['off', { allowAsProps: true }],
+  },
 };
