b5fa17b76d66715ff4edfd904988aeb615f063d0
Anderson R. Vanzo <anderson.vanzo01@gmail.com>
2023-06-28 16:09:57 -0300

    feat(redux): configurations

--- App.tsx
+++ App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import Routes from './src/routes/routes';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
+import { persistor, store } from './src/redux/store';
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Routes />
-    </NavigationContainer>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <NavigationContainer>
+          <Routes />
+        </NavigationContainer>
+      </PersistGate>
+    </Provider>
   );
 };
 
