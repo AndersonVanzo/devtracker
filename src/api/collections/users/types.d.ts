7f3cb165c1198c88bdd798a8e27dbbd55e6c15a7
Anderson R. Vanzo <anderson.vanzo01@gmail.com>
2023-06-28 17:30:20 -0300

    fix(api): fixed UserData type

--- src/api/collections/users/types.d.ts
+++ src/api/collections/users/types.d.ts
@@ -3,6 +3,6 @@ import { Endpoints } from '@octokit/types';
 export {};
 
 declare global {
-  type UserData = Endpoints['GET /users']['response']['data'];
+  type UserData = Endpoints['GET /user']['response']['data'];
   type UserRepositoryList = Endpoints['GET /users/{username}/repos']['response']['data'];
 }
