7ee913a5439a7db3d2c9853456ad8f35d6ec5ff3
Anderson R. Vanzo <anderson.vanzo01@gmail.com>
2023-06-28 17:18:52 -0300

    chore: update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,5 +5,6 @@ module.exports = {
     'object-curly-spacing': 0,
     'no-control-regex': 0,
     'react/no-unstable-nested-components': ['off', { allowAsProps: true }],
+    'react-hooks/exhaustive-deps': 'off',
   },
 };
