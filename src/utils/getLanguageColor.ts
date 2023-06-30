125e18abc85478d7e6fe9023edbda31506ced7ae
Anderson R. Vanzo <anderson.vanzo01@gmail.com>
2023-06-29 21:53:00 -0300

    feat(utils): function to get language color

--- src/utils/getLanguageColor.ts
+++ src/utils/getLanguageColor.ts
@@ -1,6 +1,6 @@
 import { colors } from '../common/colors';
 
-export const getLanguageColor = (language: string | null) => {
+export const getLanguageColor = (language: string | null | undefined) => {
   const jsonColors = require('../common/language_color.json');
   return jsonColors[language ?? ''] ?? colors.text_light;
 };
